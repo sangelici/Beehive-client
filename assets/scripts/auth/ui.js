21daea315727aea954806bb8727516628fb7be19
Mahelet.K <maheletkassa1@gmail.com>
2019-11-05 14:21:43 -0500

    Created sign up feature
    created form event listener, ajax, and ui
    issue #1
    
    Co-authored-by: Mahelet Kassa <maheletkassa1@gmail.com>

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,1 +1,22 @@
 'use strict'
+
+const store = require('../store')
+
+
+const onSignUpSuccess = function () {
+  successMessage('signed up successfully!')
+  $('#sign-up').trigger('reset')
+}
+
+const onSignUpFailure = function () {
+  failureMessage('Sign up failed')
+  $('#sign-up').trigger('reset')
+}
+
+
+
+module.exports = {
+    onSignUpSuccess,
+    onSignUpFailure
+
+}
